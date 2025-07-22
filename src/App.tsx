57eb20728cabfeb46325d46dd01863979e2b56d4
Samuel Bitencourt <samuellodeia@gmail.com>
2025-07-22 10:40:53 -0300

    learning css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import { Heading } from './components/Heading';
 
 export function App(){
     console.log('Oi');
 
     return(
         <>
-        <h1>Hello world</h1>
+       <Heading/>
+       <Heading/>
+       <Heading/>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati natus odio repellat
              eveniet ducimus corrupti ex suscipit velit distinctio ipsam quisquam 
              eligendi quo, autem ut adipisci tempore magni aliquid omnis?</p>
